e25f3fd93d4cdc72153fb58e788fd558092c427f
Amaan-Haider <amaanhaider270@gmail.com>
2023-08-24 02:21:32 +0530

    UPDATE: made changes to make sure to crawl data for every page from the website ✅

--- server/controllers/crawler.controllers.js
+++ server/controllers/crawler.controllers.js
@@ -5,40 +5,39 @@ require("dotenv").config();
 
 const crawlAndStoreData = async (req, res) => {
   try {
-    const response = await axios.get(process.env.CRAWLING_WEBSITE_URL);
-    const $ = cheerio.load(response.data);
-    const newsData = [];
-    $('.articles').each((index, element) => {
+    let currentPage = 1;
+    while (true) {
+      const response = await axios.get(`${process.env.CRAWLING_WEBSITE_URL}/page/${currentPage}/`);
+      const $ = cheerio.load(response.data);
+      const newsData = [];
+      $('.articles').each((index, element) => {
       const article = $(element);
       const imageSrcWithResize = article.find('.snaps img').attr('src');
-      const imageSrc = imageSrcWithResize.split('?')[0];
+      const imageSrc = imageSrcWithResize ? imageSrcWithResize.split('?')[0] : null;
       const title = article.find('.title a').text();
       const date = article.find('.date').text();
       const content = article.find('.img-context p').text();
-
-    //   console.log(`Title: ${title}`);
-    //   console.log(`Date: ${date}`);
-    //   console.log(`Content: ${content}`);
-    //   console.log(`Image Source: ${imageSrc}`);
-
       newsData.push({ title, date, content, imageSrc });
     });
-
-    
-    await News.insertMany(newsData);
-    const savedData = await News.find({});
-    if (savedData.length > 0) {
-      res.json('Data has been crawled and stored in MongoDB.');
-      console.log('Data has been crawled and stored in MongoDB.');
-    } else {
-      res.status(500).json({ message: 'Data could not be saved in MongoDB' });
-      console.error('Data could not be saved in MongoDB.');
+      await News.insertMany(newsData);
+    //   console.log(`Crawled and stored data from page ${currentPage}`);
+      const nextPageLink = $('.ie-pagination a.next').attr('href');
+      if (!nextPageLink) {
+        break;
+     }
+      currentPage++;
+      await new Promise(resolve => setTimeout(resolve, 1000));
     }
+    res.json('Data has been crawled and stored in MongoDB.');
+    console.log('All pages have been crawled and data stored in MongoDB.');
   } catch (error) {
     console.error('Error crawling and storing data:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+
+
+
 
 const getCrawledNews = async(req,res)=>{
     try {
